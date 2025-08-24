a114cd8fa3b72b685e30b103652d9f4691b5bbd8
Khizar2014-cyber <mohdk1525@gmail.com>
2025-08-24 17:37:22 +0000

    DataTypes.js

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,16 @@
+"using strict"; // treat all js code as a newer version
+
+console.log("hello world")
+
+console.log(3+3)
+
+let name = "khizar"
+var age = 20
+let loggedIn = false
+
+
+console.log (typeof 20); //to know the type of data example var const let//
+console.log (typeof null); // null => object
+
+console.log (typeof undefined); //undefined => undefined
+
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,6 +2,6 @@ const accountNumber = 123409
 let accountUser = "khizar"
 var accountPassword = 1234
 let accountLocation = "bhopal"
-let accountState;
+let  accountState;
 
 console.table ([accountNumber,accountUser,accountPassword, accountLocation,accountState])
