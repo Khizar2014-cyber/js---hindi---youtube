517d06b0582fcada7ac4c140d5eae0fa5d6c5ecc
Khizar2014-cyber <mohdk1525@gmail.com>
2025-08-24 17:20:38 +0000

    varibles.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountNumber = 123409
+let accountUser = "khizar"
+var accountPassword = 1234
+let accountLocation = "bhopal"
+let accountState;
+
+console.table ([accountNumber,accountUser,accountPassword, accountLocation,accountState])
