bb35f96b6d7599d868439f4b751c65b83e20a7e6
Khizar2014-cyber <mohdk1525@gmail.com>
2025-09-01 06:51:09 +0000

    changes

---
+++ 01_basics/operator.js
@@ -0,0 +1,33 @@
+console.log("6"+1);
+console.log(1+"6")
+console.log(2+2+"5");
+console.log("5"+2+2);
+
+console.log(true);
+console.log(false);
+
+let statement = false
+console.log(typeof statement);
+
+let num1 , num2 , num3 ,num
+
+num1=num2=num3=num=3+3
+
+console.log(num3);
+
+// gamecouter //
+
+let gameCouter = 100
+gameCouter ++;   // used in both prefix and sufix //
+console.log(gameCouter);
+
+
+
+
+
+
+
+
+
+
+
